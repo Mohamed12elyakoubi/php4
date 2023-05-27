9683013d8467b6db73ef31f6b38c92a0e0ad172e
Mohamed El Yakoubi <102755988+Mohamed12elyakoubi@users.noreply.github.com>
2023-05-27 20:32:08 +0200

    Add files via upload

---
+++ week4.php
@@ -0,0 +1,40 @@
+
+<?php
+// Opdracht1
+// for ($getallen = 0; $getallen <= 50; $getallen++) {
+//     echo $getallen ;
+// }
+
+
+// Opdracht 2
+// $klasgenoten = [
+//     "Mohamed",
+//     "Maroune",
+//     "Hamza",
+//     "Gekke turk",
+//     "White skin boy",
+//     "Milan de homo",
+//     "Zaka",
+//     "Rai",
+//     "Luca",
+//     "The one en the only de goat DON"
+// ];
+
+// foreach ($klasgenoten as $namen ) {
+//     echo $namen . "<br>";
+// }
+
+// Opdracht 3
+
+// $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+// for ($i = 0; $i < count($maanden); $i++) {
+//     echo 'Maand ' . ($i + 1) . ' is ' . $maanden[$i] . '.<br />';
+// }
+
+// Opdracht 4
+$maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+foreach ($maanden as $i => $maand) {
+    echo 'Maand ' . ($i + 1) . ' is ' . $maand . '.<br />';
+}
+?>
+
